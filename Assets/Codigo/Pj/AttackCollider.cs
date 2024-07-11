50e64bd5c9a1c91c3545d0b9ef37587e3d1bcfb2
Rinkachikikaari <magoku252@gmail.com>
2024-07-11 00:41:38 -0400

    .....

--- Assets/Codigo/Pj/AttackCollider.cs
+++ Assets/Codigo/Pj/AttackCollider.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AttackCollider : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
